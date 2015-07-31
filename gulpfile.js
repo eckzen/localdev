b4fda780efaacc7ee64ce01c3b2357ac5439d352
eckzen <eckzen@yahoo.com>
2015-07-31 16:23:26 +0800

    script OK

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var prefix = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -11,6 +18,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("_/sass/*.scss", ['sass']);
+    gulp.watch("_/js/*.js", ['script']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
@@ -20,6 +28,22 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest("_/css"))
         .pipe(browserSync.stream());
+});
+
+/* Scripts task */
+gulp.task('scripts', function() {
+  return gulp.src([
+    /* Add your JS files here, they will be combined in this order */
+    '_/js/bootstrap.js',
+    '_/js/jquery.js',
+    '_/js/myscript.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('_/components/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('_/components/js'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
